fc326e25362adec3cd514f914ad62de9ea9b659b
sua-no <sua953@naver.com>
2022-08-01 16:24:46 +0900

    Feat: nestjs 프로젝트 세팅

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = 8080;
+  await app.listen(port, () =>
+    console.log(`Server is running on port ${port}`),
+  );
 }
 bootstrap();
